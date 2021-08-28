ebd979905afc9ebd934a81d97a9a224e768ba7eb
Qodeskill <skillqode.paresh@gmail.com>
2021-08-28 10:52:55 +0530

    initial commit

---
+++ prg_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << endl
+         << "1 Hello ";
+    cout << endl
+         << "2 Hello ";
+    cout << endl
+         << "3 Hello ";
+    return 0;
+}
